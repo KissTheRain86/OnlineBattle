3ea5a4c46573c6cca3388ed615b157df0fd44cb3
zengweihao <zwh86771385@gmail.com>
2025-12-18 21:09:56 +0800

    点击控制人物移动

---
+++ OnlineBattle/Assets/Scripts/BaseHuman.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHuman : MonoBehaviour
+{
+
+    public string Desc = "";
+
+    public float Speed;
+
+    protected bool isMoving = false;
+
+    private Vector3 targetPosition;
+
+    private Animator animator;
+
+    public void MoveTo(Vector3 pos)
+    {
+        targetPosition = pos;
+        isMoving = true;
+        animator.SetBool("isMoving", true);
+    }
+
+    private void MoveUpdate()
+    {
+        if (isMoving == false) return;
+        
+        transform.position = Vector3.MoveTowards(
+            transform.position, targetPosition,
+            Speed * Time.deltaTime);
+        transform.LookAt(targetPosition);
+        if (Vector3.Distance(transform.position, targetPosition) < 0.05f)
+        {
+            isMoving = false;
+            animator.SetBool("isMoving", false);
+        }
+    }
+
+
+    protected virtual void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    protected virtual void Update()
+    {
+        MoveUpdate();
+    }
+
+}
---
+++ OnlineBattle/Assets/Scripts/CtrHuman.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrHuman : BaseHuman
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray,out RaycastHit hit);
+            if(hit.collider.tag == "Terrain")
+            {
+                MoveTo(hit.point);
+            }
+        }
+    }
+}
